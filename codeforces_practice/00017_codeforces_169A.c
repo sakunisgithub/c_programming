7d718fcb9e64e89e0427d27ace0c6aa2d76f7bac
sakunisgithub <ananda2003biswas@gmial.com>
2022-11-29 22:46:33 +0530

    Initial Commit

---
+++ codeforces_practice/00017_codeforces_169A.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+void bubble_sort(int *, int);
+
+void swap(int *, int *);
+
+int main()
+{
+    int n, a, b;
+
+    scanf("%d", &n);
+    int complexity[n];
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    for (int j = 0; j < n; j++)
+    {
+        scanf("%d", &complexity[j]);
+    }
+
+    bubble_sort(complexity, n);
+
+    printf("%d\n", complexity[a-1] - complexity[a]);
+    
+    return 0;
+}
+
+void bubble_sort(int * array, int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        int sort_code = 0;
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (*(array + j) < *(array + j + 1))
+            {
+                sort_code = 1;
+                swap((array + j), (array + j + 1));
+            }
+            
+        }
+
+        if (sort_code == 0)
+        {
+            break;
+        }
+        
+    }
+    
+}
+
+void swap(int * a, int * b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
